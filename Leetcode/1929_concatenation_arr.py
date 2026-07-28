9b52718a680c21e2bb88bb5f02b05006863abb3c
Kalisetti Sruthi <kalisettisruthi2007@gmail.com>
2026-07-28 19:47:56 +0530

    updated on 280726

---
+++ Leetcode/1929_concatenation_arr.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums,n=None):
+        if n is None :
+            n = len(nums)
+        ans = []
+        for i in range(2) :
+            ans.extend(nums)
+        return ans
