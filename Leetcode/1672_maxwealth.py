d06ff1dc0ff38ea89c77665abeb119d93c4e3eb4
Kalisetti Sruthi <kalisettisruthi2007@gmail.com>
2026-07-30 19:31:35 +0530

    270726-290726

---
+++ Leetcode/1672_maxwealth.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maximumWealth(self, accounts):
+        max_wealth = 0 
+        for customer in accounts :
+            current_customer = sum(customer)
+            if current_customer > max_wealth :
+                max_wealth = current_customer
+        return max_wealth
